62916521150e5a90c00283f29a96c95ed8bd8727
MinhHoang1819 <115516411+MinhHoang1819@users.noreply.github.com>
2023-09-11 13:47:39 +0700

    Upload file len server

---
+++ Upload_file.php
@@ -0,0 +1,15 @@
+<?php
+    if (isset($_POST["upload"])){
+        if (isset($_FILES["file"]) && !$_FILES["file"]["error"]){
+            move_uploaded_file($_FILES["file"]["tmp_name"], './media/'.$_FILES["file"]["name"]);
+        }else echo "error";
+    }
+?>
+<<html>
+    <body>
+        <form method="post" action="" enctype="multipart/form-data">
+            <input type="file" name="file"/>
+            <input type="submit" name="upload" value="Upload"/>
+        </form>
+    </body>
+</html>
